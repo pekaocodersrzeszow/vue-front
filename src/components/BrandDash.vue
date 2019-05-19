77670c9ec48890e9f89ea847202aaee2fed49133
avenal <avenalszarosiwy@gmail.com>
2019-05-19 14:18:41 +0200

    hehe

--- src/components/BrandDash.vue
+++ src/components/BrandDash.vue
@@ -9,7 +9,9 @@
           <v-card-text>
             <h2 class="text-md-center text-sm-center">Nowi klienci w tym miesiącu: 247</h2>
             <h2 class="text-md-center text-sm-center">Polecenia w tym miesiącu : 28</h2>
+            <br>
 
+            <canvas id="savings-chart" class="chart"></canvas>
           </v-card-text>
           <v-card-title class="text-xs-center">
             <h1 class="mx-auto">Aktualne rabaty <v-btn color="success">Dodaj
@@ -54,12 +56,41 @@
 
 <script>
   export default {
-    name: 'BrandDash'
+    name: 'BrandDash',
+    methods: {
+      createChart () {
+        const chart = new Chart(document.getElementById('savings-chart').getContext('2d'), {
+          // The type of chart we want to create
+          type: 'bar',
+
+          // The data for our dataset
+          data: {
+            labels: ['01/05','02/05','03/05','04/05','05/05','06/05','07/05','08/05','09/05','10/05','11/05','12/05','13/05','14/05','15/05','16/05','17/05','18/05','19/05',],
+            datasets: [{
+              label: 'Przychody w tym miesiącu',
+              backgroundColor: 'rgb(205, 99, 132)',
+              borderColor: 'rgb(255, 99, 132)',
+              data: [7993,7506,8731,6954,8478,7995,9891,7522,8522,8174,7153,8662,8554,7288,7644,7617,7307,6733,9777]
+            }]
+          },
+
+          // Configuration options go here
+          options: {},
+        })
+      }
+    },
+    mounted () {
+      this.createChart()
+  }
   }
 </script>
 
 <style scoped>
   .menu-icon{
     margin-left: 0.5rem;
+  }
+  .wrapper{
+    background-image: url("~@/assets/brand.jpg");
+    background-size: cover;
   }
 </style>
